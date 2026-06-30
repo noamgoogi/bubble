7e81814e33be245fc16ecafc8b607fd59f38c345
Noamgiga <noamgiga@gmail.com>
2026-06-30 22:16:06 +0300

    adding camera

--- main.py
+++ main.py
@@ -1,50 +1,95 @@
-import matplotlib.pyplot as plt
+import cv2
 import numpy as np
 
+cam = cv2.VideoCapture(0)
 
-def draw():
-    grid = np.zeros((size, size))
-
-    for x, y in visited:
-        grid[y][x] = 1
-
-    plt.clf()
-    plt.imshow(grid, cmap="Blues", vmin=0, vmax=1)
-    plt.pause(0.1)
-
-
-def bubble(x, y):
-    current = {(x, y)}
-
-    while current:
-        next_layer = set()
-
-        for x, y in current:
-            if (x, y) in visited:
-                continue
-
-            if x < 0 or y < 0 or x >= size or y >= size:
-                continue
-
-            visited.add((x, y))
-
-            next_layer.add((x+1, y))
-            next_layer.add((x-1, y))
-            next_layer.add((x, y+1))
-            next_layer.add((x, y-1))
-
-        draw()
-
-        current = next_layer
-
-
+size = 50
 visited = set()
-size = 21
+front = set()
+active = False
 
-plt.ion()
-plt.figure()
+cv2.namedWindow("Bubble", cv2.WINDOW_NORMAL)
+cv2.setWindowProperty("Bubble", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-bubble(size//2, size//2-5)
 
-plt.ioff()
-plt.show()
+def getAvgRGB(frame, x, y, w, h):
+    x1 = x * size
+    y1 = y * size
+
+    x2 = min(x1 + size, w)
+    y2 = min(y1 + size, h)
+
+    region = frame[y1:y2, x1:x2]
+
+    if region.size == 0:
+        return (0, 0, 0)
+
+    avg_color = np.mean(region, axis=(0, 1))
+    return tuple(map(int, avg_color))
+
+
+def step_bubble(w, h):
+    global front, visited, active
+
+    if not front:
+        active = False
+        return
+
+    next_front = set()
+
+    for x, y in front:
+        if (x, y) in visited:
+            continue
+
+        if x < 0 or y < 0 or x >= w // size or y >= h // size:
+            continue
+
+        visited.add((x, y))
+
+        for n in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
+            if n not in visited:
+                next_front.add(n)
+
+    front = next_front
+
+
+def draw(frame, w, h):
+    for x, y in front:
+        color = getAvgRGB(frame, x, y, w, h)
+        cv2.rectangle(frame, (x * size, y * size), (x * size + size, y * size + size), color, -1)
+
+
+def mouse_event(event, mx, my, flags, param):
+    global front, visited, active, w, h
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if active:
+            return
+
+        grid_x = mx // size
+        grid_y = my // size
+
+        visited = set()
+        front = {(grid_x, grid_y)}
+        active = True
+
+
+cv2.setMouseCallback("Bubble", mouse_event)
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        break
+
+    h, w = frame.shape[:2]
+
+    step_bubble(w, h)
+    draw(frame, w, h)
+
+    cv2.imshow("Bubble", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
